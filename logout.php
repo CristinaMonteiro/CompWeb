0b71c97db151ead1aeb48232e520f85864e5bac5
Cristina Monteiro <77967823+CristinaMonteiro@users.noreply.github.com>
2022-05-28 19:22:22 +0100

    Privilégios; Administradores + Users
    Adição de privilégios.
    Falta adicionar a interface

---
+++ logout.php
@@ -0,0 +1,17 @@
+<!--When the user clicks on the log out or sign out link, the script inside this 
+file destroys the session and redirect the user back to the login page.-->
+
+<?php
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
